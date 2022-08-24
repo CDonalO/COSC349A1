43e5496920863d017b5b6372500133c868e41c66
Jacob Rowe <rowja703@student.otago.ac.nz>
2022-08-25 11:20:40 +1200

    Get connection to database working most likely.

--- adminwebserver_files/www/index.php
+++ adminwebserver_files/www/index.php
@@ -3,8 +3,9 @@
 $servername = "192.168.12.42";
 $username = "adminprivilege";
 $password = "password1239";
+$dbname = "skybnb";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     print_r("Error");
