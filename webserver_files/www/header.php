979f41591ecb5dd49161285c296f7615a802eea0
Jacob Rowe <rowja703@student.otago.ac.nz>
2022-08-25 18:00:36 +1200

    add base for user website

---
+++ webserver_files/www/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title>SkyBNB</title>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+<header>
+    <h1>SkyBNB</h1>
+
+    <div id="user">
+            <div id="login">
+                <form id="loginForm" method="post">
+                    <label for="loginUser">Username: </label>
+                    <input type="text" name="loginUser" id="loginUser"><br>
+                    <label for="loginPassword">Password: </label>
+                    <input type="password" name="loginPassword" id="loginPassword"><br>
+                    <input type="submit" id="loginSubmit" value="Login">
+                </form>
+            </div>
+    </div>
+
+</header>
