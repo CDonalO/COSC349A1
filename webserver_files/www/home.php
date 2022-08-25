13bc68cf8723139d57e1c0a9f1ea550a8bf2c814
Jacob Rowe <rowja703@student.otago.ac.nz>
2022-08-25 20:19:09 +1200

    pull house objects from database and display them

--- webserver_files/www/home.php
+++ webserver_files/www/home.php
@@ -10,18 +10,31 @@ if ($conn->connect_error) {
     print_r("Error");
 }
 
-$sql = "SELECT * FROM Houses";
-$result = $conn->query($sql);
-print_r($result);
-
 include("header.php");
 ?>
 
-<main>
+    <main>
+        <?php
+        $sql = "SELECT * FROM Houses where(approved='false')";
+        $result = $conn->query($sql);
+        $obj = $result->fetch_object();
+        while($obj != ""){
+            echo "<ul class='house'>";
+//            echo "<li><img src="$obj->image" width='240' height='180'></li>";
+            echo "<li> $obj->bedrooms bedrooms</li>";
+            echo "<li>$obj->beds beds</li>";
+            echo "<li>$obj->guest_limit guest maximum</li>";
+            echo "<li>$obj->bathrooms bathrooms</li>";
+            echo "<li>$$obj->price_per_day per day</li>";
+            echo "<li>$$obj->cleaning_fee for cleaning</li>";
+            echo "<li>$obj->description</li>";
+            echo "<li>Located at $obj->address, $obj->city $obj->country</li>";
+            echo "</ul>";
+            $obj = $result->fetch_object();
+        }
 
-<p>Hello</p>
-
-</main>
+        ?>
+    </main>
 <?php
 include("footer.php");
 ?>
