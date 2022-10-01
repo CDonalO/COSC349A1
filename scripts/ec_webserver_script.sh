f4bd44839ea6c343d8f8a291e2afe9a583b27eed
Cordell O'Leary <cordelloleary@gmail.com>
2022-10-01 18:49:25 +1300

    added ec2 scripts

--- scripts/ec_webserver_script.sh
+++ scripts/ec_webserver_script.sh
@@ -2,7 +2,7 @@ apt-get update
 apt-get install -y apache2 php libapache2-mod-php php-mysql
 cd /home/ubuntu/
 git clone https://github.com/CDonalO/COSC349A1.git
-cp COSC349A1/adminwebserver_files/www/* /var/www/html/
+cp COSC349A1/webserver_files/www/* /var/www/html/
 rm /var/www/html/index.html
 rm -r COSC349A1
 service apache2 restart
