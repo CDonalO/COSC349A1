22511360e04b27072b7f51d0a828066ddf514d62
Jacob Rowe <rowja703@student.otago.ac.nz>
2022-10-01 18:51:06 +1300

    give user permission in script for images to work

--- scripts/ec_webserver_script.sh
+++ scripts/ec_webserver_script.sh
@@ -5,4 +5,5 @@ git clone https://github.com/CDonalO/COSC349A1.git
 cp COSC349A1/webserver_files/www/* /var/www/html/
 rm /var/www/html/index.html
 rm -r COSC349A1
+usermod -a -G ubuntu www-data
 service apache2 restart
